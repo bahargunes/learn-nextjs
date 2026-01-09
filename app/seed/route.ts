0c919902f094ae77dc9949248321d1b683a7b2a0
Bahar GÜNEŞ <bahar.gunes@tbtk.gov.tr>
2026-01-09 16:06:43 +0300

    add runtime

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,8 +1,15 @@
+export const runtime = 'nodejs';
+
+
 import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, {
+    ssl: {
+        rejectUnauthorized: false, // This bypasses some local certificate issues
+    },
+});
 
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
