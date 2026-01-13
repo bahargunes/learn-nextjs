83ce3a3d6e9f3abca58c0d76f31d39c8d8ec175a
Bahar GÜNEŞ <bahar.gunes@tbtk.gov.tr>
2026-01-13 09:08:13 +0300

    chapter 9-fix

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -12,7 +12,6 @@ import { fetchRevenue } from '@/app/lib/data';
 
 export default async function RevenueChart() { // Make component async, remove the props
     const revenue = await fetchRevenue(); // Fetch data inside the component 
-{
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
